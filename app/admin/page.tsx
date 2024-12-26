bc5578e9b344220f19486bc4347953009618899c
erain <enesyagmur99@gmail.com>
2024-12-27 02:20:15 +0300

    key i gizlemek için öncelikle firebase işlemleri yapıldı, son olarak giriş yapmadan admin sayfasına gidildiğinde geri dönmesi için kontrol eklendi

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+"use client";
+
+import { auth } from "@/firebase/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Admin = () => {
+  const router = useRouter();
+
+  const checkUser = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.back();
+      } else {
+        console.log(user);
+      }
+    });
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
   return <div>Admin</div>;
 };
 
