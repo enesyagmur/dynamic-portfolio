b0e06ec894d4f1a436d7f9ce40f0adeda50f9643
erain <enesyagmur99@gmail.com>
2025-01-28 12:17:49 +0300

    detay sayfasına link bölümü eklendi

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -3,6 +3,10 @@
 import React, { useEffect, useState } from "react";
 import projectsData from "../../../public/data/projects-overwiev.json";
 
+import { TbWorld } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+
 interface DetailParams {
   params: Promise<{
     id: string;
@@ -60,11 +64,39 @@ const Detail: React.FC<DetailParams> = ({ params }) => {
           </div>
 
           <div className="w-full flex flex-col items-center justify-center">
-            <div className="w-full flex flex-col my-4">
-              <p className="text-xl font-bold m-2">Açıklama</p>
-              <p className="p-2 text-neutral-300">
-                {project.description.title}
-              </p>
+            <div className="w-full flex items-center justify-between my-8">
+              <div className="w-2/12 h-full flex flex-col items-start justify-start md:justify-center">
+                <div className="w-full h-2/5 flex items-start md:items-center justify-start mb-2">
+                  <p className="text-xl font-bold">Linkler</p>
+                </div>
+
+                <div className="w-full h-2/5 flex items-start md:items-center justify-start text-lg">
+                  <Link
+                    href={project.live}
+                    className="mr-4 text-purple-500  hover:text-neutral-500"
+                  >
+                    <TbWorld />
+                  </Link>
+
+                  <Link
+                    href={project.github}
+                    className="mr-4 text-blue-500  hover:text-neutral-500"
+                  >
+                    <FaGithub />
+                  </Link>
+                </div>
+              </div>
+              <div className="w-8/12 md:w-10/12 flex flex-col">
+                <div className="w-full h-2/5 flex items-center justify-start mb-2">
+                  <p className="text-xl font-bold">Açıklama</p>
+                </div>
+
+                <div className="w-full h-2/5 flex items-center justify-start">
+                  <p className="text-sm text-neutral-300">
+                    {project.description.title}
+                  </p>
+                </div>
+              </div>
             </div>
 
             <div className="w-full flex md:flex-row flex-col">
